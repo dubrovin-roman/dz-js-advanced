27cd36cd373fac3e972f60ed9959e2bab1ed096f
Roman D. <romario.skfo@outlook.com>
2026-04-01 23:26:07 +0300

    dz done

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
 'use strict';
 
-const panel = document.querySelector('.panel');
+// Базовый класс Персонаж
+function Character(species, name, language) {
+  this.species = species;
+  this.name = name;
+  this.language = language;
+}
 
-setInterval(() => {
-  const now = new Date();
-  const year = now.getFullYear();
-  const month = now.getMonth();
-  const currentSeconds = now.getSeconds();
-  const currentMinutes = now.getMinutes();
-  const currentHours = now.getHours();
-  const currentDay = now.getDate();
-  const lastDayOfMonth = new Date(year, month + 1, 0).getDate();
-  const secondsUntilEndOfMinute = 60 - currentSeconds;
-  const minutesUntilEndOfHour = 60 - currentMinutes;
-  const hoursUntilEndOfDay = 24 - currentHours;
-  const daysLeftInMonth = lastDayOfMonth - currentDay;
-  const monthsUntilEndOfYear = 11 - month;
-  panel.innerText = `${monthsUntilEndOfYear} месяцев, ${daysLeftInMonth} дней, ${hoursUntilEndOfDay} часов, ${minutesUntilEndOfHour} минут, ${secondsUntilEndOfMinute} секунд`;
-}, 1000);
+Character.prototype.speak = function() {
+  console.log(`language: ${this.language} name: ${this.name}`);
+};
+
+
+// Класс Орк
+function Orc(name, weapon) {
+  Character.call(this, 'Орк', name, 'орочий');
+  this.weapon = weapon;
+}
+Orc.prototype = Object.create(Character.prototype);
+Orc.prototype.constructor = Orc;
+Orc.prototype.strike = function() {
+  console.log(`${this.species} по имени ${this.name} нанес удар ${this.weapon}`);
+}
+
+const ork1 = new Orc('Ваня', 'Дубина');
+ork1.speak();
+ork1.strike();
+
+//Класс Эльф
+function Elf(name, typeOfSpells) {
+  Character.call(this, 'Эльф', name, 'эльфский');
+  this.typeOfSpells = typeOfSpells;
+}
+Elf.prototype = Object.create(Character.prototype);
+Elf.prototype.constructor = Elf;
+Elf.prototype.createSpell = function() {
+  console.log(`${this.species} по имени ${this.name} создал заклнание ${this.typeOfSpells}`);
+}
+
+const elf1 = new Elf('Леголаз', 'Огненный шар');
+elf1.speak();
+elf1.createSpell();
