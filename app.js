750edcc1f82dd3b4333554dfdefb63962c7337ca
Roman D. <dubrovin.skfo@gmail.com>
2026-04-18 22:19:28 +0300

    dz done

--- app.js
+++ app.js
@@ -1,43 +1,32 @@
 "use strict";
 
-const request = new XMLHttpRequest();
-request.open("GET", "https://pokeapi.co/api/v2/pokemon/ditto");
-request.onerror = function () {
-  console.log("Ошибка сети");
-};
-request.send();
-
-request.addEventListener("load", function () {
-  if (this.status >= 200 && this.status < 300) {
-    try {
-      const { abilities } = JSON.parse(this.responseText);
-      if (!Array.isArray(abilities) || abilities.length === 0) {
-        return;
-      }
-      const url = abilities[0].ability.url;
-      const rq = new XMLHttpRequest();
-      rq.open("GET", url);
-      rq.send();
-      rq.addEventListener("load", function () {
-        if (this.status >= 200 && this.status < 300) {
-          try {
-            const { effect_entries } = JSON.parse(this.responseText);
-            if (!Array.isArray(effect_entries) || effect_entries.length === 0) {
-              return;
-            }
-            const enRes = effect_entries.find((e) => e.language.name === "en");
-            console.log(enRes.effect);
-          } catch (error) {
-            console.error("Invalid JSON");
-          }
-        } else {
-          console.log("Ошибка сервера:", this.status);
-        }
-      });
-    } catch (error) {
-      console.error("Invalid JSON");
+fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error("HTTP " + res.status);
     }
-  } else {
-    console.log("Ошибка сервера:", this.status);
-  }
-});
+    return res.json();
+  })
+  .then((data) => {
+    const { abilities } = data;
+    if (!Array.isArray(abilities) || abilities.length === 0) {
+      throw new Error("Invalid abilities");
+    }
+    const url = abilities[0].ability.url;
+    return fetch(url);
+  })
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error("HTTP " + res.status);
+    }
+    return res.json();
+  })
+  .then((data) => {
+    const { effect_entries } = data;
+    if (!Array.isArray(effect_entries) || effect_entries.length === 0) {
+      throw new Error("Invalid effect_entries");
+    }
+    const enRes = effect_entries.find((e) => e.language.name === "en");
+    console.log(enRes.effect);
+  })
+  .catch((err) => console.log(err));
